f59e262e21e08c0e8bed11aa581ca5ce43666b74
Ziyad <ziyadrff@gmail.com>
2023-12-03 09:57:10 +0700

    Added a clear terminal function to delete outputs at terminal after iteration or after it called.

--- randomizer_test.py
+++ randomizer_test.py
@@ -7,14 +7,26 @@ start = True
 running = False
 word = None
 end_prog = False
+clear_terminal = False
 
 def run_program():
     global running, word
     running = not running
     if not running:
-        print(f"YOU GOT : {word}")
+        print(f"\nYOU GOT : {word}")
         print("\nPress 'Space' key to start and stop randomizing...")
+        print("Press 'C' to clear terminal...")
         print("Press 'X' to end program...")
+
+def reset_terminal():
+    global clear_terminal
+    clear_terminal = not clear_terminal
+    if clear_terminal:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Press 'Space' key to start and stop randomizing...")
+        print("Press 'C' to clear terminal...")
+        print("Press 'X' to end program...")
+
 
 def stop_program():
     global end_prog
@@ -22,8 +34,10 @@ def stop_program():
 
 keyboard.add_hotkey('space', run_program)
 keyboard.add_hotkey('X', stop_program)
+keyboard.add_hotkey('C', reset_terminal)
 
 print("Press 'Space' key to start and stop randomizing...")
+print("Press 'C' to clear terminal...")
 print("Press 'X' to end program...")
 
 while start:
